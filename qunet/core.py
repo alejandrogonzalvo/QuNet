c8c3957c8e1680bcbd30834fbd62138938d24d53
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2024-01-01 12:13:14 +0100

    change edge list to work with references instead of ids

--- qunet/core.py
+++ qunet/core.py
@@ -4,14 +4,15 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import json
 import qunet.id_manager as id_manager
+from copy import deepcopy
 
 class Core:
     id: int = 0
     qubits: list[Qubit] = []
-    edges: list[tuple[int, int]] = []
+    edges: list[tuple[Qubit, Qubit]] = []
     gates: list[Gate] = []
 
-    def __init__(self, qubits: list[Qubit]=[], edges: list[tuple[int, int]]=[], gates: list[Gate]=[]):
+    def __init__(self, qubits: list[Qubit]=[], edges: list[tuple[Qubit, Qubit]]=[], gates: list[Gate]=[]):
         self.id = id_manager._core_id
         id_manager._core_id += 1
         self.qubits = qubits
@@ -28,17 +29,27 @@ class Core:
             }
         }
     
-    def add_qubit(self, q: Qubit) -> None:
+    def _get_edges(self) -> list[tuple[int, int]]:
+        return [(edge[0].id, edge[1].id) for edge in self.edges]
+    
+    def add_qubit(self, q: Qubit) -> int:
         if q in self.qubits:
             raise Exception(f"qubit with ID={q.id} already exists in Core with ID={self.id}")
 
         self.qubits.append(q)
+        return q.id
+    
+    def get_qubit(self, id: int) -> Qubit:
+        for qubit in self.qubits:
+            if qubit.id == id:
+                return qubit
+        raise Exception(f"Qubit with ID={id} not found in Core with ID={self.id}")
 
     def plot(self):
         G = nx.Graph()
         for qubit in self.qubits:
             G.add_node(qubit.id)
-        G.add_edges_from(self.edges)
+        G.add_edges_from(self._get_edges())
         nx.draw(G, with_labels=True)
         plt.show()
     
@@ -54,11 +65,27 @@ class Core:
         
         c : Core = Core()
         for i in range(total):
-            c.add_qubit(Qubit())
+            id = c.add_qubit(Qubit())
 
             if i % x != 0:
-                c.edges.append((i-1, i))
+                c.edges.append((c.get_qubit(id-1), c.get_qubit(id)))
             if i >= x:
-                c.edges.append((i-x, i))
+                c.edges.append((c.get_qubit(id-x), c.get_qubit(id)))
         
+        return c
+    
+    @staticmethod
+    def copy(core):
+        c: Core = deepcopy(core)
+        c.id = id_manager._core_id
+        id_manager._core_id += 1
+
+        for qubit in c.qubits:
+            qubit.id = id_manager._qubit_id
+            id_manager._qubit_id += 1
+        
+        for gate in c.gates:
+            gate.id = id_manager._gate_id
+            id_manager._gate_id += 1
+
         return c
--- qunet/network.py
+++ qunet/network.py
@@ -8,9 +8,9 @@ from copy import deepcopy
 class Network:
     id: int = 0
     cores: list[Core] = []
-    edges: list[tuple[int, int]] = []
+    edges: list[tuple[Core, Core]] = []
 
-    def __init__(self, cores: list[Core]=[], edges: list[tuple[int, int]]=[]):
+    def __init__(self, cores: list[Core]=[], edges: list[tuple[Core, Core]]=[]):
         self.id = id_manager._network_id
         id_manager._network_id += 1
         self.cores = cores
@@ -25,17 +25,25 @@ class Network:
             }
         }
     
-    def add_core(self, c: Core) -> None:
+    
+    def add_core(self, c: Core) -> int:
         if c in self.cores:
             raise Exception(f"core with ID={c.id} already exists in Network with ID={self.id}")
 
         self.cores.append(c)
+        return c.id
+    
+    def get_core(self, id: int) -> Core:
+        for core in self.cores:
+            if core.id == id:
+                return core
+        raise Exception(f"core with ID={id} does not exist in Network with ID={self.id}")
 
     def plot(self):
         G = nx.Graph()
         for core in self.cores:
             G.add_node(core.id)
-        G.add_edges_from(self.edges)
+        G.add_edges_from(self._get_edges())
         nx.draw(G, with_labels=True)
         plt.show()
 
@@ -46,13 +54,12 @@ class Network:
         n : Network = Network()
         n.add_core(core)
         for i in range(1, total):
-            n.add_core(Core(deepcopy(core.qubits), deepcopy(core.edges), deepcopy(core.gates)))
+            id: int = n.add_core(Core.copy(core))
 
             if i % x != 0:
-                n.edges.append((i-1, i))
+                n.edges.append((n.get_core(id), n.get_core(id-1)))
             if i >= x:
-                n.edges.append((i-x, i))
+                n.edges.append((n.get_core(id-x), n.get_core(id)))
         
         return n
-
     
